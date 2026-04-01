77288581d9694dce525781151ddb9056ec8ef80a
EDIN-hash <0662982401w@gmail.com>
2026-04-01 18:34:39 +0200

    test: history with params again

--- src/neon-client.js
+++ src/neon-client.js
@@ -330,8 +330,8 @@ const NeonClient = {
     async getHistory(itemName = null) {
         try {
             if (itemName && itemName.trim()) {
-                const query = `SELECT * FROM history_log WHERE item_name = '${itemName}' LIMIT 100`;
-                return await neonQuery(query);
+                const query = `SELECT * FROM history_log WHERE item_name = $1 LIMIT 100`;
+                return await neonQuery(query, [itemName]);
             } else {
                 const query = `SELECT * FROM history_log LIMIT 200`;
                 return await neonQuery(query);
